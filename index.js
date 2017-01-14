955e570e931458407aa164fec43b23196d90879a
Jakub Niewczas <niewczas.jakub@gmail.com>
2017-01-14 14:55:14 +0100

    advertise gitServer port + keep local gitServer up all the time

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const gitServerPort = process.env.GIT_SERVER_PORT || 7000
 const localReposStoragePath = '.tmp/repos/'
 
 const net = NodeDiscover()
+net.advertise({
+  gitServerPort: gitServerPort
+});
+
+const gatewayServer = new GatewayServer(gatewayServerPort)
+
 const gitServer = new GitServer({
   port: gitServerPort,
   storagePath: localReposStoragePath,
@@ -14,34 +20,30 @@ const gitServer = new GitServer({
     net.send('update-available', {repoName: repoName})
   }
 })
-const gatewayServer = new GatewayServer(gatewayServerPort)
-gatewayServer.restartWithTargetUrl('http://localhost:' + gitServerPort)
+gitServer.start()
+.then(() => {
+  console.log('local http git server started')
+})
 
 net.on('promotion', () => {
   console.log('I was promoted to a master.')
 
   net.leave('update-available')
-  gitServer.start()
-  .then(() => {
-    console.log('local http git server started')
-  })
-  .then(() => {
-    gatewayServer.restartWithTargetUrl('http://localhost:' + gitServerPort)
-  })
+  gatewayServer.restartWithTargetUrl('http://localhost:' + gitServerPort)
+  console.log('reconfiguring gateway, entry address: http://localhost:' + gatewayServerPort + ' (pointing at: '+ gatewayServer.getTargetUrl() +')')
 })
 
 net.on('demotion', () => {
   console.log('I was demoted from being a master.')
-  gitServer.stop()
-  .then(() => {
-    console.log('local http git server stopped')
-  })
 })
 
 net.on('master', (obj) => {
   console.log('A new master is in control')
+
   const newGitMasterServerIp = obj.address
-  gatewayServer.restartWithTargetUrl('http://' + newGitMasterServerIp + ':' + gitServerPort)
+  const newGitMasterServerPort = obj.advertisement.gitServerPort
+  gatewayServer.restartWithTargetUrl('http://' + newGitMasterServerIp + ':' + newGitMasterServerPort)
+  console.log('reconfiguring gateway, entry address: http://localhost:' + gatewayServerPort + ' (pointing at: '+ gatewayServer.getTargetUrl() +')')
 
   net.join('update-available', (data) => {
     gitServer.mirrorRepo(gatewayServer.getTargetUrl(), data.repoName)
