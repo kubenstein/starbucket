74caeb8448440c6aa929e96ac1fc3e98eb2be40f
Jakub Niewczas <niewczas.jakub@gmail.com>
2017-05-29 13:22:53 +0200

    upgrade webpack to 2.6.1

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -30,19 +30,21 @@ module.exports = {
   },
 
   module: {
-    loaders: [
+    rules: [
       {
         test: /(\.js)$/,
-        loader: 'babel',
-        query: {
-          presets: ['stage-0', 'es2015']
-        }
+        use: [{
+          loader: 'babel-loader',
+          query: {
+            presets: ['stage-0', 'es2015']
+          }
+        }]
       }
     ]
   },
 
   resolve: {
-    root: [
+    modules: [
       srcDir,
       rootDir
     ]
