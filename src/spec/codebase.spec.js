6baeaeac77b49e452fc4de46634efae89c106b1d
Jakub Niewczas <niewczas.jakub@gmail.com>
2017-07-05 21:20:20 +0200

    simplify linter spec

--- src/spec/codebase.spec.js
+++ src/spec/codebase.spec.js
@@ -1,16 +1,13 @@
 'use strict'
 
-const spawn = require('child_process').spawn
+const exec = require('child_process').exec
 
 describe('Starbucket Codebase', function () {
   this.timeout(20000)
 
   it('follows JS Standard code style', (done) => {
-    let errorHappened = false
-    let output = ''
-    const proccess = spawn('npm', ['run', 'dev:linter'])
-    proccess.on('exit', () => { errorHappened ? done(output) : done() })
-    proccess.stdout.on('data', (data) => { output += data })
-    proccess.stderr.on('data', (data) => { errorHappened = true })
+    exec('npm run dev:linter', (error, stdout) => {
+      error ? done(stdout) : done()
+    })
   })
 })
